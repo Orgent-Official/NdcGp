7cd3eacbda360920878ffa3a6eca0e5d5aa28792
Shen Yanchen <155559960+Orgent-Official@users.noreply.github.com>
2025-08-26 14:15:15 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,98 @@
-var _3l=0;
+var _3l=8; //chpc
 var utw=1620;
 var ndc=600;
 var cwmm=926;
 var chy=100;
-var nacl=20;
+var nacl=410; //ptc
 var _5u=121;
 var eug=370;
+
+function makeop()
+{
+	var option = {
+			title: {
+				text: '市值走势图'
+			},
+			tooltip: {
+				trigger: 'axis'
+			},
+			legend: {
+				data: ['CHPC', 'Utw', 'Ndc','CWMM','Eug','Chy','PTC','5U']
+			},
+			grid: {
+				left: '3%',
+				right: '4%',
+				bottom: '10%',
+				containLabel: true
+			},
+			xAxis: {
+				type: 'category',
+				boundaryGap: false,
+				data: ['8.20', '8.21', '8.22', '8.23', '8.24', '8.25', '8.26', '当前']
+			},
+				yAxis: {
+				type: 'value'
+			},
+			series: [
+			{
+				name: 'CHPC',
+				type: 'line',
+				data: [21,20,18,15,12,9,8,_3l]
+			},
+			{
+				name: 'Utw',
+				type: 'line',
+				data: [1954,1742,1756,1699,1600,1600,1640,utw]
+			},
+			{
+				name: 'Ndc',
+				type: 'line',
+				data: [491,492,495,497,502,520,550,ndc]
+			},
+			{
+				name: 'CWMM',
+				type: 'line',
+				data: [1021,1000,950,900,880,921,922,cwmm]
+			},
+			{
+				name: 'Eug',
+				type: 'line',
+				data: [449,430,390,380,360,370,350,eug]
+			},
+			{
+				name: 'Chy',
+				type: 'line',
+				data: [132,119,98,76,43,50,85,chy]
+			},
+			{
+				name: 'PTC',
+				type: 'line',
+				data: [270,230,280,350,461,521,400,nacl]
+			},
+			{
+				name: '5U',
+				type: 'line',
+				data: [117,116,117,118,116,110,120,_5u]
+			}
+		]
+		};
+	return option;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var l1=130,l2=1540,l3=499,l4=920,l5=370,l6=43,l7=165,l8=116;
 
 
@@ -45,36 +132,7 @@ function buychecknowprice(id)
 
 function buycheckpastprice(id)
 {
-	switch(Number(id))
-	{
-	case 0:
-		return 0;
-		break;
-	case 1:
-		return l1;
-		break;
-	case 2:
-		return l2;
-		break;
-	case 3:
-		return l3;
-		break;
-	case 4:
-		return l4;
-		break;
-	case 5:
-		return l5;
-		break;
-	case 6:
-		return l6;
-		break;
-	case 7:
-		return l7;
-		break;
-	case 8:
-		return l8;
-		break;
-	}
+	return id;
 }
 
 function buycheckname(id)
@@ -85,28 +143,28 @@ function buycheckname(id)
 		return "无";
 		break;
 	case 1:
-		return "3L  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "CHPC";
 		break;
 	case 2:
-		return "Utw  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "Utw";
 		break;
 	case 3:
-		return "Ndc  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "Ndc";
 		break;
 	case 4:
-		return "CWMM  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "CWMM";
 		break;
 	case 5:
-		return "Eug  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "Eug";
 		break;
 	case 6:
-		return "Chy  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "Chy";
 		break;
 	case 7:
-		return "氯化钠  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "PTC";
 		break;
 	case 8:
-		return "5U  (" + String(buychecknowprice(id)-buycheckpastprice(id)) + ") 编号：" + String(id);
+		return "5U";
 		break;
 	}
 
